e36376792ef119262ada47bc5cbe76644dd241b0
zcjie <1192052751@qq.com>
2023-04-14 14:03:43 +0800

    测试合并

--- src/main/java/com/zhengcj/webdemo/WebDemoApplication.java
+++ src/main/java/com/zhengcj/webdemo/WebDemoApplication.java
@@ -3,6 +3,11 @@ package com.zhengcj.webdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @description: 启动类
+ * @author: zcjie
+ * @date: 2023/4/14 14:03
+ **/
 @SpringBootApplication
 public class WebDemoApplication {
 
